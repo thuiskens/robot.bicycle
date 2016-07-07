b311d83748719f0710c76af607ef303aedcc8107
Oliver Lee <oliverzlee@gmail.com>
2016-07-07 11:45:10 +0200

    Search OSX paths in FindMatlab CMake module
    Change Matlab library search path for OSX.

--- cmake/Modules/FindMatlab.cmake
+++ cmake/Modules/FindMatlab.cmake
@@ -1,27 +1,30 @@
-
-set(MATLAB_ROOT_BIN ${MATLAB_ROOT}/bin/glnxa64)
+if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    set(MATLAB_LIBRARY_DIR ${MATLAB_ROOT}/bin/maci64)
+else()
+    set(MATLAB_LIBRARY_DIR ${MATLAB_ROOT}/bin/glnxa64)
+endif()
 
 find_library(MATLAB_MEX_LIBRARY
              mex
-             ${MATLAB_ROOT_BIN})
+             ${MATLAB_LIBRARY_DIR})
 
 find_library(MATLAB_MX_LIBRARY
              mx
-             ${MATLAB_ROOT_BIN})
+             ${MATLAB_LIBRARY_DIR})
 
 find_library(MATLAB_ENG_LIBRARY
              eng
-             ${MATLAB_ROOT_BIN})
+             ${MATLAB_LIBRARY_DIR})
 
 find_library(MATLAB_UT_LIBRARY
-             ut 
-             ${MATLAB_ROOT_BIN})
+             ut
+             ${MATLAB_LIBRARY_DIR})
 
 find_library(MATLAB_MAT_LIBRARY
              mat
-             ${MATLAB_ROOT_BIN})
+             ${MATLAB_LIBRARY_DIR})
 
-find_path(MATLAB_INCLUDE_DIR 
+find_path(MATLAB_INCLUDE_DIR
           "mex.h"
           ${MATLAB_ROOT}/extern/include)
 
@@ -34,7 +37,9 @@ set(MATLAB_LIBRARIES
 )
 
 if(MATLAB_INCLUDE_DIR AND MATLAB_LIBRARIES)
-  set(MATLAB_FOUND 1)
+    set(MATLAB_FOUND 1)
+else()
+    message(SEND_ERROR "MATLAB not found. MATLAB dependent targets cannot be built.")
 endif()
 
 mark_as_advanced(
@@ -47,4 +52,3 @@ mark_as_advanced(
   MATLAB_INCLUDE_DIR
   MATLAB_FOUND
   MATLAB_ROOT)
-
